6940d15d0f4a9e6a00cf4e20a0211d26c1a6ba5c
yagita <takashi.yagita@ibm.com>
2020-02-08 13:09:41 +0900

    1. 基本ベースを作成

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # AmazonClowdWatchiLogsNotification
-AmazonClowdWatchiLogsNotification
+
+1. Set variables (terminal)
+
+```bash
+$ PROJECTNAME=opstools
+$ ROLENAME=logsAlarm
+$ LAMBDAUPLOADBUCKETNAME=xxxxxxxxxxxx
+$ MAILADDRESS=xxxxx@xxx.xxx
+```
+
+
+
+PROJECTNAME=opstools
+ROLENAME=logsAlarm
+LAMBDAUPLOADBUCKETNAME=yagita-lambda-bucket-ap-northeast-1
+MAILADDRESS=yagita.takashi@gmail.com
+
+
+
+
+
+
+2. Upload local artifacts(Lambda)
+```bash
+$ aws cloudformation package \
+    --template-file template.yml \
+    --s3-bucket $LAMBDAUPLOADBUCKETNAME \
+    --output-template-file packaged.yml
+```
+
+3. Deploys the specified AWS CloudFormation template
+```bash
+$ aws cloudformation deploy \
+    --stack-name $PROJECTNAME-$ROLENAME \
+    --region ap-northeast-1 \
+    --template-file packaged.yml \
+    --capabilities CAPABILITY_NAMED_IAM \
+    --output text \
+    --parameter-overrides \
+        NortificationMail=$MAILADDRESS
+```
---
+++ test_lambda/lambda_function.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+import os
+import time
+import datetime
+import logging
+import boto3
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+logs = boto3.client('logs')
+
+# environment variable
+LOGS_GROUP_NAME = os.environ['LOGS_GROUP_NAME']
+LOGS_STREAM_NAME = os.environ['LOGS_STREAM_NAME']
+
+
+def put_log_events(message):
+
+    # events format
+    events = [
+        dict([('timestamp', int(time.time())*1000), ('message', message)])
+    ]
+
+    response = logs.describe_log_streams(
+        logGroupName=LOGS_GROUP_NAME,
+        logStreamNamePrefix=LOGS_STREAM_NAME)
+
+    logger.info('describe_log_streams: {}'.format(response))
+
+    stream = response['logStreams'][0]
+    sequence_token = stream.get('uploadSequenceToken')
+
+    if sequence_token:
+        logs.put_log_events(
+            logGroupName=LOGS_GROUP_NAME,
+            logStreamName=LOGS_STREAM_NAME,
+            logEvents=events,
+            sequenceToken=sequence_token
+        )
+    else:
+        logs.put_log_events(
+            logGroupName=LOGS_GROUP_NAME,
+            logStreamName=LOGS_STREAM_NAME,
+            logEvents=events
+        )
+    logger.info('logs.put_log_event: {}'.format(events))
+
+
+def lambda_handler(event, context):
+    logger.info('event: {}'.format(event))
+
+    try:
+        for _ in range(100):
+
+            # message = '2020-02-07T00:00:00.123456 Error foo bar'
+            message = '{} Error foo bar'.format(datetime.datetime.now().strftime("%Y/%m/%dT%H:%M:%S"))
+            put_log_events(message)
+            time.sleep(1/4)
+
+    except Exception as e:
+        logger.error(e)
+        raise e
